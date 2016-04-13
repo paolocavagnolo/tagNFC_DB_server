c1ff316850073b3438673f683ed713772a6d6138
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-13 13:22:11 +0200

    go

--- Gateway/rasp_read_serial.py
+++ Gateway/rasp_read_serial.py
@@ -1,7 +1,12 @@
 import serial
 
-port = serial.Serial()
-port.baudrate = 115200
-port.port = '/dev/ttyAMA0'
-port.open()
-print(port.is_open)
+#link with moteino
+moteino = serial.Serial()
+moteino.baudrate = 115200
+moteino.port = '/dev/ttyAMA0'
+moteino.timeout = 3
+moteino.open()
+print(moteino)
+
+if (moteino.readlines):
+    print "ciao"
