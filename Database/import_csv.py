95114d678e48f84b8c670ac9246157cd55ed6cee
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-13 16:32:45 +0200

    go

--- Database/import_csv.py
+++ Database/import_csv.py
@@ -7,8 +7,8 @@ with codecs.open("out.json", "w", encoding="utf-8") as out:
     for r in reader:
         for k, v in r.items():
          # make sure nulls are generated
-        if not v:
-            r[k] = None
+         if not v:
+             r[k] = None
          # generate int
         elif k == 'Tessera':
             r[k] = int(v)
