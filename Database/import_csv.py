c0ee19829bfa982001bf3198e9c65e97a92eac08
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-13 16:51:53 +0200

    go

--- Database/import_csv.py
+++ Database/import_csv.py
@@ -4,7 +4,16 @@ data = open("soci.csv")
 reader = csv.DictReader(data, delimiter=",", quotechar='"')
 
 with codecs.open("out.json", "w", encoding="utf-8") as out:
-    print reader
+    for r in reader:
+        for k, v in r.items():
+            if not v:
+                r[k] = None
+            elif k == 'Tessera':
+                r[k] = int(v)
+    out.write(simplejson.dumps(r, ensure_ascii=False, use_decimal=True)+"\n")
+
+
+
     # for r in reader:
     #     for k, v in r.items():
     #      # make sure nulls are generated
