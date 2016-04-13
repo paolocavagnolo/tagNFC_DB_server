7ee812d29d41031cb1750a59300c3deb72b076d8
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-13 16:31:42 +0200

    go

--- Database/import_csv.py
+++ Database/import_csv.py
@@ -7,9 +7,9 @@ with codecs.open("out.json", "w", encoding="utf-8") as out:
     for r in reader:
         for k, v in r.items():
          # make sure nulls are generated
-         if not v:
+        if not v:
             r[k] = None
          # generate int
-        elif k == "Tessera":
+        elif k == 'Tessera':
             r[k] = int(v)
         out.write(simplejson.dumps(r, ensure_ascii=False, use_decimal=True)+"\n")
