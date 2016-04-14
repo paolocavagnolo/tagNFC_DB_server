26c27338dbe4bfdf5a6649b3f237fc72a504e0ba
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-14 21:43:56 +0200

    go

--- Database/read_gsheet.js
+++ Database/read_gsheet.js
@@ -6,14 +6,6 @@ var col = parseInt(process.argv[3]);
 var row = parseInt(process.argv[2]);
 
 
-CLIENT_ID = '1083549263547-2rp85g51in8kl864ch7nisoehepk2odu.apps.googleusercontent.com';
-CLIENT_SECRET = 'c7APBFFW55IVgp5BAvMZYT7b';
-REDIRECT_URL = 'techlab.tl';
-
-// OPTIONAL: if you want to perform authenticated requests.
-// You must install this dependency yourself if you need it.
-var google = require('googleapis');
-
 GoogleSpreadsheets({
     key: '1gBOByKgaDgbneWOBalxZ5jihJVV1iUFKuytqGVUG380',
   }, function(err, spreadsheet) {
