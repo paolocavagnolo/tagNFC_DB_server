3c30b824347266b64b49cffaa422768a49c52b15
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-14 11:43:05 +0200

    go

--- Database/sync_gsheet.js
+++ Database/sync_gsheet.js
@@ -14,7 +14,7 @@ Spreadsheet.load({
 
     if (err) throw err;
 
-    spreadsheet.add({ 3: { 2: process.argv[1] } });
+    spreadsheet.add({ 3: { 2: process.argv[2] } });
 
     spreadsheet.send(function(err) {
       if(err) throw err;
