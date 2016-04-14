dfdc0a1050b062b7a51737798d1e6f40a2d17f09
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-14 22:25:20 +0200

    go

--- Database/import_csv.py
+++ Database/import_csv.py
@@ -8,5 +8,4 @@ with codecs.open("/home/pi/Documents/Database/out.json", "w", encoding="utf-8") 
         for k, v in r.items():
             if not v:
                 r[k] = None
-
-    out.write(simplejson.dumps(r, ensure_ascii=False, use_decimal=True)+"\n")
+            out.write(simplejson.dumps(r, ensure_ascii=False, use_decimal=True)+"\n")
