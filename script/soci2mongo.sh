52ff82a089851af9a8f93c8614d79ef7ebfb07f3
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-14 22:06:23 +0200

    go

--- script/soci2mongo.sh
+++ script/soci2mongo.sh
@@ -5,7 +5,7 @@ cd /home/pi/Documents/Database/
 rake ohana:csv[https://docs.google.com/spreadsheets/d/1gBOByKgaDgbneWOBalxZ5jihJVV1iUFKuytqGVUG380/edit?usp=sharing]
 
 echo "csv to json"
-python /home/pi/Documents/Database/csv/import_csv.py
+python /home/pi/Documents/Database/import_csv.py
 
 echo "json to mongoDB"
 mongoimport --db techlab --collection soci --drop --file /home/pi/Documents/Database/out.json
