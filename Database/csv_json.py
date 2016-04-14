224812866595010c9c5aff2ecf4b23a1c68376e4
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-14 22:17:19 +0200

    go

--- Database/csv_json.py
+++ Database/csv_json.py
@@ -6,17 +6,17 @@ import json
 fieldnames=["Tessera","tagNFC","Crediti","Data richiesta","Data accettazione ","Tutore","Mail","Nome","Cognome","Data nascita","Luogo nascita","Residenza","CF","Qualifica","Quota 2015","Quota 2016","Data annullamento"]
 
 def convert(filename):
- csv_filename = filename[0]
- print "Opening CSV file: ",csv_filename
- f=open(csv_filename, 'r')
- csv_reader = csv.DictReader(f,fieldnames)
- json_filename = csv_filename.split(".")[0]+".json"
- print "Saving JSON to file: ",json_filename
- jsonf = open(json_filename,'w')
- data = json.dumps([r for r in csv_reader])
- jsonf.write(data)
- f.close()
- jsonf.close()
+    csv_filename = filename[0]
+    print "Opening CSV file: ",csv_filename
+    f=open(csv_filename, 'r')
+    csv_reader = csv.DictReader(f,fieldnames)
+    json_filename = csv_filename.split(".")[0]+".json"
+    print "Saving JSON to file: ",json_filename
+    jsonf = open(json_filename,'w')
+    data = json.dumps([r for r in csv_reader])
+    jsonf.write(data)
+    f.close()
+    jsonf.close()
 
 if __name__=="__main__":
  convert(sys.argv[1:])
