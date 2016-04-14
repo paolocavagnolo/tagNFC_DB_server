6750a3a5ec7a47518a6193b906c23a8f812a9817
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-14 16:38:40 +0200

    go

--- tagNFC_server.py
+++ tagNFC_server.py
@@ -6,6 +6,8 @@ while True:
     try:
         linea = ser.readline()
         if (linea != ""):
-            print linea.split(",")[4].split(" ")
+            for item in linea.split(",")[4].split(" "):
+                print item.split("x").[1]
+
     except (KeyboardInterrupt, SystemExit):
         ser.close()
