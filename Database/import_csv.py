789040f7e2d3d1c1532eb736bf5aed2fb520800f
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-14 22:23:54 +0200

    go

--- Database/import_csv.py
+++ Database/import_csv.py
@@ -8,18 +8,5 @@ with codecs.open("/home/pi/Documents/Database/out.json", "w", encoding="utf-8") 
         for k, v in r.items():
             if not v:
                 r[k] = None
-            elif k == 'Tessera':
-                r[k] = int(v)
+
     out.write(simplejson.dumps(r, ensure_ascii=False, use_decimal=True)+"\n")
-
-
-
-    # for r in reader:
-    #     for k, v in r.items():
-    #      # make sure nulls are generated
-    #      if not v:
-    #          r[k] = None
-    #      # generate int
-    #     elif k == 'Tessera':
-    #         r[k] = int(v)
-    #     out.write(simplejson.dumps(r, ensure_ascii=False, use_decimal=True)+"\n")
