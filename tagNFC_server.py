6c341cefecd12128f722ae23c81a51ca5f522e38
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-14 16:43:38 +0200

    go

--- tagNFC_server.py
+++ tagNFC_server.py
@@ -7,7 +7,7 @@ while True:
         linea = ser.readline()
         if (linea != ""):
             uid = linea.split(",")[4].split(" ")[0].split("x")[1] + linea.split(",")[4].split(" ")[1].split("x")[1] + linea.split(",")[4].split(" ")[2].split("x")[1] + linea.split(",")[4].split(" ")[3].split("x")[1]
-
+            print uid
 
     except (KeyboardInterrupt, SystemExit):
         ser.close()
