49093a484d5abdfd3e5d822e529338f66ff7bc9c
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-14 15:41:42 +0200

    go

--- tagNFC_server.py
+++ tagNFC_server.py
@@ -3,5 +3,5 @@ import serial
 ser = serial.Serial('/dev/ttyAMA0',115200,timeout=3)
 
 while True:
-    if (ser.readLine() != ""):
-        print ser.readLine()
+    if (ser.readline() != ""):
+        print ser.readline()
