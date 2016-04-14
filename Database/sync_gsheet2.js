d43f42772c58c6e07fac95183babd61305c274db
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-14 18:08:39 +0200

    go

--- Database/sync_gsheet2.js
+++ Database/sync_gsheet2.js
@@ -9,7 +9,7 @@ var data = {};
 data[row] = {};
 data[row][col] = process.argv[2];
 
-async.series([]
+async.series([
 
   function setAuth(step) {
     // see notes below for authentication instructions!
