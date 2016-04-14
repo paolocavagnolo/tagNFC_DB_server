a3eb505b45622cb33fccf7a71992cb24fb14ef35
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-14 16:46:41 +0200

    go

--- tagNFC_server.py
+++ tagNFC_server.py
@@ -5,8 +5,9 @@ ser = serial.Serial('/dev/ttyAMA0',115200,timeout=1)
 while True:
     try:
         #take
-        linea = ser.readline()
-        if (linea[0] == '#'):
+        x = ser.read()
+        if (x == '#'):
+            linea = ser.readline()
             uid = linea.split(",")[4].split(" ")[0].split("x")[1] + linea.split(",")[4].split(" ")[1].split("x")[1] + linea.split(",")[4].split(" ")[2].split("x")[1] + linea.split(",")[4].split(" ")[3].split("x")[1]
             print uid
 
