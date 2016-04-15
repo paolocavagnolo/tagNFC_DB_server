46f681b8683ccbe7cb8a1bacb62f085d0bce82ba
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-15 14:45:30 +0200

    go

--- tagNFC_server.py
+++ tagNFC_server.py
@@ -7,6 +7,7 @@ scope = ['https://spreadsheets.google.com/feeds']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/pi/techlab-tag-nfc-b3f2a2929d98.json', scope)
 gc = gspread.authorize(credentials)
 sht1 = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
+worksheet = sh.get_worksheet(0)
 
 #serial part
 ser = serial.Serial('/dev/ttyAMA0',115200,timeout=1)
@@ -25,7 +26,7 @@ while True:
             print uid
 
         if (cc == 1):
-            print muterun_js('/home/pi/Database/read_gsheet.js 2 3')
+            print worksheet.cell(3, 2).value
             cc = 0;
 
     except (KeyboardInterrupt, SystemExit):
