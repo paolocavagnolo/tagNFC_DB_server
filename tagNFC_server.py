2dae7cfb965fb53154d98943500d925e1411b85f
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-15 17:26:06 +0200

    go

--- tagNFC_server.py
+++ tagNFC_server.py
@@ -41,9 +41,9 @@ while True:
             else:
                 ser.write('o')
                 ser.write(',')
-                ser.write(unicode(worksheet.cell(cell.row, 3).value))
+                ser.write((int)(worksheet.cell(cell.row, 3).value))
                 ser.write(',')
-                ser.write(unicode(worksheet.cell(cell.row, 4).value))
+                ser.write((int)(worksheet.cell(cell.row, 4).value))
             tag = 0;
 
     except (KeyboardInterrupt, SystemExit):
