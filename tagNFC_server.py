df8f19aba1bd580aa0e4676af7295f64e51eefcf
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-04-16 15:21:24 +0200

    go

--- tagNFC_server.py
+++ tagNFC_server.py
@@ -37,7 +37,7 @@ while True:
                 ser.write('n')
             else:
                 print "PY: Find one!"
-                ser.write('o' + worksheet.cell(cellTag.row, 3).value + worksheet.cell(cellTag.row, 4).value)
+                ser.write('o' + (bytes)worksheet.cell(cellTag.row, 3).value + (bytes)worksheet.cell(cellTag.row, 4).value)
                 print "PY: Credits:"
                 print worksheet.cell(cellTag.row, 3).value
                 print "PY: Skills:"
