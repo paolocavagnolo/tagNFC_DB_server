0e85094d941a6a33ce75c0d29aa5c42b329b184d
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-07 14:24:59 +0200

    go

--- tagNFC_server.py
+++ tagNFC_server.py
@@ -62,27 +62,6 @@ def byte2float( bytess ):
 
     return float("{0:.2f}".format(struct.unpack('>f', b)[0]))
 
-def readDeliveryData ( line ):
-
-    abss = int(linea.split(",")[1])
-    ids = int(linea.split(",")[2])
-    idr = int(linea.split(",")[3])
-    message[0] = linea.split(",")[4]
-    message[1] = linea.split(",")[5]
-    message[2] = linea.split(",")[6]
-    if (len(message[2])<2):
-        message[2] = '0'+message[2]
-    message[3] = linea.split(",")[7]
-    if (len(message[3])<2):
-        message[3] = '0'+message[3]
-    message[4] = linea.split(",")[8]
-    if (len(message[4])<2):
-        message[4] = '0'+message[4]
-    message[5] = linea.split(",")[9]
-    if (len(message[5])<2):
-        message[5] = '0'+message[5]
-    RSSI = int(linea.split(",")[10])
-
 while True:
     # Gspread!
     try:
@@ -115,7 +94,24 @@ while True:
                 linea = ser.readline()
                 print linea
                 #read delivery data
-                readDeliveryData( linea )
+                abss = int(linea.split(",")[1])
+                ids = int(linea.split(",")[2])
+                idr = int(linea.split(",")[3])
+                message[0] = linea.split(",")[4]
+                message[1] = linea.split(",")[5]
+                message[2] = linea.split(",")[6]
+                if (len(message[2])<2):
+                    message[2] = '0'+message[2]
+                message[3] = linea.split(",")[7]
+                if (len(message[3])<2):
+                    message[3] = '0'+message[3]
+                message[4] = linea.split(",")[8]
+                if (len(message[4])<2):
+                    message[4] = '0'+message[4]
+                message[5] = linea.split(",")[9]
+                if (len(message[5])<2):
+                    message[5] = '0'+message[5]
+                RSSI = int(linea.split(",")[10])
                 print ids
                 #recode
                 if (ids == 4):
