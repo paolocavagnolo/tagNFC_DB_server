5526c47e58e890adfc6940472925c4579cc9b4da
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-10 17:18:01 +0200

    go

--- serial2file.py
+++ serial2file.py
@@ -8,6 +8,10 @@ ser = serial.Serial('/dev/ttyAMA0',115200,timeout=1)
 buff = open(filename,'a+')
 
 while True:
-    if (ser.inWaiting() > 0):
-        linea = ser.readline()
-        print linea
+    try:
+        if (ser.inWaiting() > 0):
+            linea = ser.readline()
+            print linea
+    except (KeyboardInterrupt, SystemExit):
+        ser.close()
+        print "good close"
