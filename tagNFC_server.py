5e7de86ec54ea0f9361f98f6adeb2c30a48f0b40
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-12 13:20:37 +0200

    go

--- tagNFC_server.py
+++ tagNFC_server.py
@@ -11,7 +11,7 @@ import struct
 print "Trovi dei cavagnolo? %r" % excel.find("cavagnolo")
 print "Trovi dei Cavagnolo? %r" % excel.find("cavagnolo")
 print "Cosa c\'e\' in 3,2? %r" % excel.read(3,2)
-print "Scrivi qualcosa in 3,3? %r" excel.write(3,3,52)
+#print "Scrivi qualcosa in 3,3? %r" excel.write(3,3,52)
 
 
 ser.close()
