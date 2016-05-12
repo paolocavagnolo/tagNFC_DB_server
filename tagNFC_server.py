42406caf028e085d95844fe6c4047b8a51b77027
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-12 13:55:50 +0200

    go

--- tagNFC_server.py
+++ tagNFC_server.py
@@ -8,16 +8,24 @@ import serial_mod as ser
 import datetime
 import struct
 
+print "#"*20
+
 print "test gdrive"
 print "Trovi dei Cavagnolo? %r" % excel.find("Cavagnolo")
 print "Cosa c\'e\' in 5,2? %r" % excel.read(3,2)
 print "Scrivi qualcosa in 3,3? %r" % excel.write(3,3,52)
 
-print "-"*10
+print "#"*20
 
 print "test mongodb"
-print "Trovi dei 2? quanti? %r cosa? %r" % db.read({"ids": 2})
+print "Trovi dei 2? quanti? %r cosa? %r" % db.read({"ids": 4})
 print "Aggiungi un documento semplice semplice? %r" % db.write({"nome": "paolo", "cognome": "cavagnolo"})
+
+print "#"*20
+
+print "test serial"
+print "qualcosa in seriale? %r" % ser.read()
+
 
 
 ser.close()
