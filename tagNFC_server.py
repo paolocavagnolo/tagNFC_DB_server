fc22443d16dd96cb8fe1b39a2cfb6347da103f42
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-12 13:22:47 +0200

    go

--- tagNFC_server.py
+++ tagNFC_server.py
@@ -8,10 +8,10 @@ import serial_mod as ser
 import datetime
 import struct
 
-print "Trovi dei cavagnolo? %r" % excel.find("cavagnolo")
+
 print "Trovi dei Cavagnolo? %r" % excel.find("cavagnolo")
 print "Cosa c\'e\' in 3,2? %r" % excel.read(3,2)
-#print "Scrivi qualcosa in 3,3? %r" excel.write(3,3,52)
+print "Scrivi qualcosa in 3,3? %r" % excel.write(3,3,52)
 
 
 ser.close()
