350cb41483b3866634ae201159b0c4fe27d7f4f6
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-13 18:09:31 +0200

    go

--- serial_mod.py
+++ serial_mod.py
@@ -1,13 +1,13 @@
 import serial
 import sys
 
-ser = serial.Serial('/dev/ttyAMA0',115200,timeout=1)
-
 def readline():
-
+    ser = serial.Serial('/dev/ttyAMA0',115200,timeout=1)
     line = ser.readline()
+    ser.close()
     return line
 
 def writeline( message ):
-
+    ser = serial.Serial('/dev/ttyAMA0',115200,timeout=1)
     ser.write(message)
+    ser.close()
--- tagNFC_server.py
+++ tagNFC_server.py
@@ -107,6 +107,5 @@ try:
 
 except KeyboardInterrupt:
     db.close()
-    ser.close()
 
     print "\nBye"
