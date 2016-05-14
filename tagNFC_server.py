8108d62dae11fe111f29fac848ff8188a603be90
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-14 17:08:58 +0200

    go

--- tagNFC_server.py
+++ tagNFC_server.py
@@ -80,7 +80,7 @@ try:
                     #'d':           #'h':               #'l': laser tick    #'p': 3d print tick     #'t': timeout   #'x':
                     ser.write('i'+incoming.__dict__['ids']+'\0')
                     time.sleep(1)
-                    ser.write('j'+float2bytes(float('1.1'))+'0'+'\0')
+                    ser.write('j'+float2bytes(float('-1.1'))+'0'+'\0')
                 else:
                     user = excel.read_row(cellTag.row)
                     #0: id      #4: Data rich   #8: Nome        #12: Residenza  #16: Quota 2016
