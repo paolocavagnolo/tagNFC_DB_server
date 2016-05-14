2bdd15e7f68b0e591159dbe3999f24ff647ad1bf
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-14 17:26:00 +0200

    go

--- gdrive_mod.py
+++ gdrive_mod.py
@@ -6,29 +6,29 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/pi/Documents/techlab-tag-nfc-b3f2a2929d98.json', scope)
-
+gc = gspread.authorize(credentials)
 
 
 def find( string ):
-    gc = gspread.authorize(credentials)
+
     sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
     worksheet = sh.worksheet("soci")
     return worksheet.find(string)
 
 def read_one( row, col ):
-    gc = gspread.authorize(credentials)
+
     sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
     worksheet = sh.worksheet("soci")
     return worksheet.cell(row, col).value
 
 def read_row( row ):
-    gc = gspread.authorize(credentials)
+
     sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
     worksheet = sh.worksheet("soci")
     return worksheet.row_values(row)
 
 def write( row, col, value):
-    gc = gspread.authorize(credentials)
+    
     sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
     worksheet = sh.worksheet("soci")
     return worksheet.update_cell(row, col, value)
