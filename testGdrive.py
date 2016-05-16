04050ec52b59be5fd4a0b73c93dcb66146fd020c
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-16 09:31:06 +0200

    go

--- testGdrive.py
+++ testGdrive.py
@@ -2,5 +2,5 @@ import gdrive_mod as gdrive
 import time
 
 while True:
-    open('testGdrive.txt','a+').write(gdrive.read_one(5,6))
+    open('testGdrive.txt','a+').write(gdrive.read_one(5,6)+' '+(time.strftime("%H:%M:%S"))+'\n')
     time.sleep(30)
