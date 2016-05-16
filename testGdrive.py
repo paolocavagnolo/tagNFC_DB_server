e99609a56b1e4b02a53dad08fb534ae67dd48189
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-16 18:38:03 +0200

    go

--- testGdrive.py
+++ testGdrive.py
@@ -1,4 +1,5 @@
 #import gdrive_mod as gdrive
+import time
 import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 import gdrive_mod as gdrive
@@ -8,19 +9,19 @@ scheduler = BackgroundScheduler()
 
 now = datetime.datetime.now()
 
-
-def open_gdrive():
-    print "ciao_fun"
+def gdrive_open():
+    gdrive.open()
+    print "renew!"
 
 #
 # while True:
 #     gdrive.open(
 #     open('testGdrive.txt','a+').write(gdrive.read_one(5,6)+' '+(time.strftime("%H:%M:%S"))+'\n')
 #     time.sleep(30)
-job = scheduler.add_job(gdrive.open, 'interval', minutes=1)
+job = scheduler.add_job(gdrive_open, 'interval', seconds=30)
 
 scheduler.start()
 
 
 while True:
-    open('testGdrivex.txt','a+').write(gdrive.read_one(5,5)+' '+now.strftime("%Y-%m-%d %H:%M"))
+    open('testGdrivex.txt','a+').write(gdrive.read_one(5,5)+' '+now.strftime("%Y-%m-%d %H:%M")+'\n')
