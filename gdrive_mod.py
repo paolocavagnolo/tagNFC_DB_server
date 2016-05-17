da85e8662336cd5f4d3318768f1148ec9aba65b4
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-17 20:18:12 +0200

    go

--- gdrive_mod.py
+++ gdrive_mod.py
@@ -4,53 +4,112 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/pi/Documents/techlab-tag-nfc-b3f2a2929d98.json', scope)
-gc = gspread.authorize(credentials)
-sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
 
-
-def open():
-    print "#"*20
-    print "REOPENED DRIVE!!!!"
-    print "#"*20
-    scope = ['https://spreadsheets.google.com/feeds']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/pi/Documents/techlab-tag-nfc-b3f2a2929d98.json', scope)
-    gc = gspread.authorize(credentials)
-    sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
 
 def find( string ):
+    try:
+        gc = gspread.authorize(credentials)
+        sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
+        if credentials.access_token_expired:
+            gc.login()
 
-    worksheet = sh.worksheet("soci")
-    return worksheet.find(string)
+        ###function
+        worksheet = sh.worksheet("soci")
+        return worksheet.find(string)
+
+    except Exception, e:
+        traceback.print_exc()
+
 
 def read_one( row, col ):
+    try:
+        gc = gspread.authorize(credentials)
+        sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
+        if credentials.access_token_expired:
+            gc.login()
 
-    worksheet = sh.worksheet("soci")
-    return worksheet.cell(row, col).value
+        ###function
+        worksheet = sh.worksheet("soci")
+        return worksheet.cell(row, col).value
+
+    except Exception, e:
+        traceback.print_exc()
+
 
 def read_row( row ):
+    try:
+        gc = gspread.authorize(credentials)
+        sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
+        if credentials.access_token_expired:
+            gc.login()
 
-    worksheet = sh.worksheet("soci")
-    return worksheet.row_values(row)
+        ###function
+        worksheet = sh.worksheet("soci")
+        return worksheet.row_values(row)
+
+    except Exception, e:
+        traceback.print_exc()
+
 
 def read_col_log( col ):
+    try:
+        gc = gspread.authorize(credentials)
+        sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
+        if credentials.access_token_expired:
+            gc.login()
 
-    worksheet = sh.worksheet("log")
-    return worksheet.col_values(col)
+        ###function
+        worksheet = sh.worksheet("log")
+        return worksheet.col_values(col)
+
+    except Exception, e:
+        traceback.print_exc()
+
 
 def read_row_log( row ):
+    try:
+        gc = gspread.authorize(credentials)
+        sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
+        if credentials.access_token_expired:
+            gc.login()
 
-    worksheet = sh.worksheet("log")
-    return worksheet.row_values(row)
+        ###function
+        worksheet = sh.worksheet("log")
+        return worksheet.row_values(row)
+
+    except Exception, e:
+        traceback.print_exc()
+
 
 def write( row, col, value):
+    try:
+        gc = gspread.authorize(credentials)
+        sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
+        if credentials.access_token_expired:
+            gc.login()
 
-    worksheet = sh.worksheet("soci")
-    return worksheet.update_cell(row, col, value)
+        ###function
+        worksheet = sh.worksheet("soci")
+        return worksheet.update_cell(row, col, value)
+
+    except Exception, e:
+        traceback.print_exc()
+
 
 def write_log( row, col, value):
+    try:
+        gc = gspread.authorize(credentials)
+        sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
+        if credentials.access_token_expired:
+            gc.login()
 
-    worksheet = sh.worksheet("log")
-    return worksheet.update_cell(row, col, value)
+        ###function
+        worksheet = sh.worksheet("log")
+        return worksheet.update_cell(row, col, value)
+
+    except Exception, e:
+        traceback.print_exc()
+
 
 def update_linea( row , linea, titoli):
 
@@ -61,12 +120,44 @@ def update_linea( row , linea, titoli):
             print "e lo confronto con %r" % titolo
             j = j + 1
             if item == titolo:
-                write_log(row,j,linea[item])
+                try:
+                    gc = gspread.authorize(credentials)
+                    sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
+                    if credentials.access_token_expired:
+                        gc.login()
+
+                    ###function
+                    write_log(row,j,linea[item])
+
+                except Exception, e:
+                    traceback.print_exc()
+
 
 def read_session( row, col):
-    worksheet = sh.worksheet("open_session")
-    return worksheet.cell(row, col).value
+    try:
+        gc = gspread.authorize(credentials)
+        sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
+        if credentials.access_token_expired:
+            gc.login()
+
+        ###function
+        worksheet = sh.worksheet("open_session")
+        return worksheet.cell(row, col).value
+
+    except Exception, e:
+        traceback.print_exc()
+
 
 def write_session( row, col, value):
-    worksheet = sh.worksheet("open_session")
-    return worksheet.update_cell(row, col, value)
+    try:
+        gc = gspread.authorize(credentials)
+        sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1KWxCi7tny8uxo4TmzjNnVuNj5eGRVngwFD2gxIX5qfw/edit?usp=sharing')
+        if credentials.access_token_expired:
+            gc.login()
+
+        ###function
+        worksheet = sh.worksheet("open_session")
+        return worksheet.update_cell(row, col, value)
+
+    except Exception, e:
+        traceback.print_exc()
