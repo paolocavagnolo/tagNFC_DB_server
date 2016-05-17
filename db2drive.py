17f931395997a3f2f8ceeb7b14d2854a04b82d8d
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-17 09:05:02 +0200

    go

--- db2drive.py
+++ db2drive.py
@@ -1,25 +1,25 @@
 import gdrive_mod as excel
-import mongodb_mod as db
+# import mongodb_mod as db
 
 # read the first line
 titoli = excel.read_row_log(1)
 
-#match key with column
-def update_linea( row , linea):
-    j = 0
-    for item in linea:
-        print "guardo %r" % item,
-        for titolo in titoli:
-            print "e lo confronto con %r" % titolo
-            j = j + 1
-            if item == titolo:
-                excel.write_log(row,j,linea[item])
-
-
-records = db.read_last_N(20)
-
-l = 1
-
-for document in records:
-    update_linea(l, document)
-    l = l + 1
+# #match key with column
+# def update_linea( row , linea):
+#     j = 0
+#     for item in linea:
+#         print "guardo %r" % item,
+#         for titolo in titoli:
+#             print "e lo confronto con %r" % titolo
+#             j = j + 1
+#             if item == titolo:
+#                 excel.write_log(row,j,linea[item])
+#
+#
+# records = db.read_last_N(20)
+#
+# l = 1
+#
+# for document in records:
+#     update_linea(l, document)
+#     l = l + 1
