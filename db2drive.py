8333bc0f4159fdac0002f2b7580451902a210b4d
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-17 09:18:07 +0200

    go

--- db2drive.py
+++ db2drive.py
@@ -6,18 +6,18 @@ titoli = excel.read_row_log(1)
 
 #match key with column
 def update_linea( row , linea):
-    j = 0
+
     for item in linea:
         print "guardo %r" % item,
+        j = 0
         for titolo in titoli:
             print "e lo confronto con %r" % titolo
             j = j + 1
             if item == titolo:
-                print "col # %r + contenuto %r " % (j,linea[item])
-                #excel.write_log(2,j,linea[item])
+                excel.write_log(2,j,linea[item])
 
 
-records = db.read_last_N(1)
+records = db.read_last_N(10)
 
 l = 2
 
