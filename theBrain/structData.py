1192131acd22292b6f6d713e145a5a4b8ee819a3
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-24 23:00:20 +0200

    go

--- theBrain/structData.py
+++ theBrain/structData.py
@@ -1,7 +1,4 @@
 import struct
-import serial
-import logging
-
 
 class radioPkt(object):
     def __init__(self, payload):
@@ -52,21 +49,3 @@ def bytes2float( data ):
     b = ''.join(chr(i) for i in bytecc)
 
     return float("{0:.2f}".format(struct.unpack('>f', b)[0]))
-
-msg1 = '<,5977,4,1,-41,65,63,B9,8A,B9,42,>'
-msg2 = '<,5969,2,1,-44,6E,3B,C2,72,62,33,81,>\r\n'
-
-ser = serial.Serial('/dev/ttyAMA0',115200)
-logging.basicConfig()
-
-try:
-    while True:
-        pl = ser.readline()
-        if (pl[0] == '<'):
-            print "letto %r di lunghezza %r" % (pl, len(pl))
-            a_msg = radioPkt(pl)
-            print a_msg.__dict__
-
-except:
-    ser.close()
-    print "bye"
--- theBrain/theBrain.py
+++ theBrain/theBrain.py
@@ -2,6 +2,7 @@
 
 from gDriveAPI import *
 from mongoDB import *
+from strucData import *
 
 ## The logging Part ##
 
@@ -17,11 +18,20 @@ gLog = gDriveAPI('log','tag_system') #open the worksheet 'log' on the file 'tag_
 gSes = gDriveAPI('open_session','tag_system')
 gUser = gDriveAPI('soci','tag_system')
 
-dbLog = mongoDB('radio_logs','techlab-db') #work with the collection 'radio-logs' with the database 'techlab-db'
-dbEnergy = mongoDB('energy','techlab-db')
-#
-# ser = serial.Serial('/dev/ttyAMA0',115200) #open a serial connection to talk with the gateway
-#
-# ## The information driver! The cool stuff. ##
-#
-# a_msg = radioPkt(payload)
+dbLog = mongoDB('radio_log','techlab') #work with the collection 'radio-logs' with the database 'techlab-db'
+dbEnergy = mongoDB('energy','techlab')
+
+ser = serial.Serial('/dev/ttyAMA0',115200) #open a serial connection to talk with the gateway
+
+## The information driver! The cool stuff. ##
+
+msg1 = '<,5977,4,1,-41,65,63,B9,8A,B9,42,>'
+msg2 = '<,5969,2,1,-44,6E,3B,C2,72,62,33,81,>\r\n'
+
+a_msg = radioPkt(msg1)
+
+print a_msg.__dict__
+
+dbLog.close()
+dbEnergy.close()
+ser.close()
