aa8308d17563404bdc1e8b5b96497c2fc5d56ffe
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-05-31 11:42:35 +0200

    go

--- theBrain/structData.py
+++ theBrain/structData.py
@@ -27,8 +27,7 @@ class answer(radioPkt):
         self.date = datetime.datetime.now()
         if self.idm == 'n' or self.idm == 't':
             self.cr = cr
-            self.cr_b = unicode(float2bytes(float(cr)),errors = 'replace')
-            self.sk = sk
+            self.cr_b = float2bytes(float(cr)).encode()
 
 
 class session(object):
