fb14685a3e1340bb562bbb16da509b0fc597bd1f
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-06-01 18:52:59 +0200

    go

--- theBrain/setEnergyCount.py
+++ theBrain/setEnergyCount.py
@@ -5,6 +5,16 @@ prompt = '> '
 
 ser = serial.Serial('/dev/ttyAMA0',115200) #open a serial connection to talk with the gateway
 
+def float2bytes( data ):
+    vals = list(struct.pack('<f', data))
+    c_vals = []
+    for num in vals:
+        if num == '\x00':
+            c_vals.append('0')
+        else:
+            c_vals.append(num)
+
+    return ''.join(c_vals)
 
 print "Count for A?"
 a_count = raw_input(prompt)
@@ -26,15 +36,3 @@ c_count = raw_input(prompt)
 ser.write('i'+'4'+'\0')
 time.sleep(1)
 ser.write('j'+float2bytes(float(c_count))+'\0')
-
-
-def float2bytes( data ):
-    vals = list(struct.pack('<f', data))
-    c_vals = []
-    for num in vals:
-        if num == '\x00':
-            c_vals.append('0')
-        else:
-            c_vals.append(num)
-
-    return ''.join(c_vals)
